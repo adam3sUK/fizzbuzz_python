2a9657eb2cca230d798d84de498e055cbf8b65d2
Adam Harbisher <aharbisher@gmail.com>
2022-06-06 11:53:45 +0100

    Completed challenge with all passing tests

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,18 +1,18 @@
-from pickletools import UP_TO_NEWLINE
-
-
-def generate(upto):
+def fizzbuzz(upto):
   generate_string = ""
   while upto > 0:
     upto_string = str(upto)
-    if upto % 3 == 0:
+    if upto % 15 == 0:
+      upto_string = "FizzBuzz"
+    elif upto % 3 == 0:
       upto_string = "Fizz"
     elif upto % 5 == 0:
       upto_string = "Buzz"
-
-    if len(generate_string) == 0:
-      generate_string = upto_string
-    else:
-      generate_string = f"{upto_string}, {generate_string}"
+    
+    generate_string = upto_string if len(generate_string) == 0 else f"{upto_string}, {generate_string}"
     upto -= 1
+
   return generate_string
+
+
+print(fizzbuzz(100))
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,24 +1,24 @@
 # For: test_fizzbuzz.py
 import unittest  # This is the default Python testing library, unittest
 
-from fizzbuzz import generate
+from fizzbuzz import fizzbuzz
 # We import our generate function (def) from the fizzbuzz module (file)
 
 class TestFizzbuzz(unittest.TestCase):  # Sets up a new test case
     def test_lists_values_up_to_one(self):  # This is a test, don't forget `self`!
-        self.assertEqual(generate(1), "1")  # And an assertion
+        self.assertEqual(fizzbuzz(1), "1")  # And an assertion
 
     def test_lists_values_up_to_two(self):
-      self.assertEqual(generate(2), "1, 2")
+      self.assertEqual(fizzbuzz(2), "1, 2")
 
     def test_lists_values_up_to_three(self):
-      self.assertEqual(generate(3), "1, 2, Fizz")
+      self.assertEqual(fizzbuzz(3), "1, 2, Fizz")
 
     def test_list_values_up_to_five(self):
-      self.assertEqual(generate(5), "1, 2, Fizz, 4, Buzz")
+      self.assertEqual(fizzbuzz(5), "1, 2, Fizz, 4, Buzz")
 
     def test_list_values_up_to_fifteen(self):
-      self.assertEqual(generate(15), "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
+      self.assertEqual(fizzbuzz(15), "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz")
 
     # Start by uncommenting the above, and when you've made that pass move 
     # forward with your own tests.
